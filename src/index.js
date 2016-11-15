0d69362c82a5a205ef5372af693e9535cb8f8515
Michael Ko <michael@faide.net>
2016-11-15 05:38:13 -0700

    Remove extension requirement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { compose, createStore } from 'redux';
+import { createStore } from 'redux';
 import { persistStore } from 'redux-persist-immutable';
 import trinketsApp from './store/reducers';
 import difference from 'lodash/difference';
@@ -9,11 +9,11 @@ import App from './App';
 import './index.css';
 import '../node_modules/@blueprintjs/core/dist/blueprint.css';
 
-const enhancer = compose(
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+// const enhancer = compose(
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
 
-const store = enhancer(createStore)(trinketsApp);
+const store = createStore(trinketsApp);
 const persistor = persistStore(store);
 
 let currentState = null;
